1869196d5e6020cf365216d208c771af0221e1f9
SaiKrishna1545 <s538340@nwmissouri.edu>
2021-02-18 23:36:43 -0600

    done with changes

--- README.md
+++ README.md
@@ -2,10 +2,10 @@
 A one page web application that utters some speech when you click on either of the colors(purple and sky blue).
 
 ## Team members
-Krishna sumanth - Client
+Pruthvi - Client
 
-Deekshith - Developer
+Sai Krishna - Developer
 
-Chandrakanth - Architect
+Deekshith- Architect
 ## Google analytics
 https://analytics.google.com/analytics/web/#/p260710849/realtime/overview?params=_u..nav%3Dgames-reporting
---
+++ locations.js
@@ -0,0 +1,22 @@
+export default [
+    {
+        name: 'Horizon  Apartments',
+        type: 'triangle',
+        coordinates: [
+            {
+                latitude: 40.3589695,
+                longitude: -94.8814606
+            },
+            {
+                latitude: 40.3589695,
+                longitude: -94.8814606
+            },
+            {
+                latitude: 40.3589695,
+                longitude: -94.8814606
+            },
+            
+        ]
+    }
+
+];
