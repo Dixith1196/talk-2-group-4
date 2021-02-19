2a10b07893f8ccb6213add1e7baa3dd68c7b6b12
SaiKrishna1545 <s538340@nwmissouri.edu>
2021-02-19 00:36:51 -0600

    updated file

--- __tests__/locationTrinagle.spec.js
+++ __tests__/locationTrinagle.spec.js
@@ -1,23 +1,3 @@
-import location from '../location.js';
 
-
-// very basic test to notify the user if our location data has changed
-test('the location data is correct', () => {
-  expect(location).toMatchSnapshot();
-  expect(location).toHaveLength(4);
-  expect(location.map(location => location.name)).toEqual([
-    'Horizon  Apartments',    
-  ]);
-});
-
-// let's test that each item in the location data has the correct properties
-for (let i = 0; i < location.length; i += 1) {
-  it(`location[${i}] should have properties (name, type, coordinates)`, () => {
-    expect(location[i]).toHaveProperty('name ');
-
-    expect(location[i]).toHaveProperty('type');
-    expect(location[i]).toHaveProperty('coordinates');
-  });
-}
 
 
